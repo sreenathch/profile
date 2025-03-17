daf9a738755ce7738530584b8a645562b0aadad1
sreenathch <sreenath.chakinala@copart.com>
2025-03-17 14:46:03 -0500

    prpfile changes

--- src/components/Home/Home.view.js
+++ src/components/Home/Home.view.js
@@ -1,26 +1,38 @@
-import React, { Component } from 'react';
-import { FaAngleDoubleDown } from 'react-icons/fa';
+import React, { Component } from "react";
+import { FaAngleDoubleDown } from "react-icons/fa";
 
-import { Intro } from './components/IntroMessage';
-import { TopBar } from './components/TopBar';
-import { AboutMeView } from './components/AboutMe';
-import { SkillsList } from './components/Skills';
-import StudyTimeLine from './components/StudyTimeLine';
-import {FileZipOutlined} from "@ant-design/icons";
-import {Icon} from "antd";
+import { Intro } from "./components/IntroMessage";
+import { TopBar } from "./components/TopBar";
+import { AboutMeView } from "./components/AboutMe";
+import { SkillsList } from "./components/Skills";
+import StudyTimeLine from "./components/StudyTimeLine";
+import { FileZipOutlined } from "@ant-design/icons";
+import { Icon } from "antd";
 
 const NewIcon = ({
-                     iconType, iconTitle, twoToneColor, value, custom = false, onClick = () => {
-    }
-                 }: string) => (
-    <div className="flex flex-row items-center items-start" onClick={onClick}>
-        {custom ? custom :
-            <Icon type={iconType} theme="twoTone" twoToneColor={twoToneColor} style={{fontSize: '34px'}}/>}
-        <h1 className="pl-3 font-sans text-lg text-grey-darkest">
-            {`${iconTitle}: `}
-        </h1>
-        <p className="pl-3 font-sans text-lg text-grey-darkest">{value}</p>
-    </div>
+  iconType,
+  iconTitle,
+  twoToneColor,
+  value,
+  custom = false,
+  onClick = () => {},
+}: string) => (
+  <div className="flex flex-row items-center items-start" onClick={onClick}>
+    {custom ? (
+      custom
+    ) : (
+      <Icon
+        type={iconType}
+        theme="twoTone"
+        twoToneColor={twoToneColor}
+        style={{ fontSize: "34px" }}
+      />
+    )}
+    <h1 className="pl-3 font-sans text-lg text-grey-darkest">
+      {`${iconTitle}: `}
+    </h1>
+    <p className="pl-3 font-sans text-lg text-grey-darkest">{value}</p>
+  </div>
 );
 
 export default class DemoApp extends Component {
@@ -33,27 +45,23 @@ export default class DemoApp extends Component {
   onNavigate = (divName: string) => {
     if (this[divName].current) {
       this[divName].current.scrollIntoView({
-        behavior: 'smooth',
-        block: 'nearest',
+        behavior: "smooth",
+        block: "nearest",
       });
     }
-  }
+  };
 
   render() {
     return (
       <div>
         <div className="parallaxLinearGradient p-3 flex flex-col">
-          <TopBar
-            navigateTo={this.onNavigate}
-          />
+          <TopBar navigateTo={this.onNavigate} />
           <Intro
             message="Hey there!! Welcome"
             name="I`m a Front End Developer mainly focusing on crafting a clean and readable code"
           />
           <div className="flex flex-col items-center">
-            <FaAngleDoubleDown
-              style={{ color: '#FFB6C1', fontSize: 40 }}
-            />
+            <FaAngleDoubleDown style={{ color: "#FFB6C1", fontSize: 40 }} />
             <p className="text-white">Scroll</p>
           </div>
         </div>
@@ -63,38 +71,58 @@ export default class DemoApp extends Component {
         <div className="pb-24 pt-4">
           <StudyTimeLine />
         </div>
-          <div className="py-10 pt-4">
-              <div className="flex flex-col items-center">
-                  <div className="border w-3/4 border-red"/>
-                  <div className="w-1/2 justify-start pb-5 aboutMeFont">
-                      <div className="flex py-10 pb-5 items-center">
-                          <p>Npm Packages :</p>
-                      </div>
-                      <div className="flex pb-5 flex-col pt-5 pl-32">
-                          <div className="justify-start pb-5">
-                              <NewIcon
-                                  custom={<FileZipOutlined theme="twoTone" twoToneColor="#12c2e9" style={{fontSize: '34px'}}/>}
-                                  iconTitle="direct-drop-area"
-                                  twoToneColor="#12c2e9"
-                                  value="https://github.com/sreenathch/get-detailed-location"
-                                  onClick={() => window.open(
-                                      "https://www.npmjs.com/package/direct-drop-area", "_blank")}
-                              />
-                          </div>
-                          <div className="justify-start pb-5">
-                              <NewIcon
-                                  custom={<FileZipOutlined theme="twoTone" twoToneColor="#12c2e9" style={{fontSize: '34px'}}/>}
-                                  iconTitle="get-detailed-location"
-                                  twoToneColor="#12c2e9"
-                                  value="https://github.com/sreenathch/direct-drop-area"
-                                  onClick={() => window.open(
-                                      "https://www.npmjs.com/package/get-detailed-location", "_blank")}
-                              />
-                          </div>
-                      </div>
-                  </div>
+        <div className="py-10 pt-4">
+          <div className="flex flex-col items-center">
+            <div className="border w-3/4 border-red" />
+            <div className="w-1/2 justify-start pb-5 aboutMeFont">
+              <div className="flex py-10 pb-5 items-center">
+                <p>Npm Packages :</p>
               </div>
+              <div className="flex pb-5 flex-col pt-5 pl-32">
+                <div className="justify-start pb-5">
+                  <NewIcon
+                    custom={
+                      <FileZipOutlined
+                        theme="twoTone"
+                        twoToneColor="#12c2e9"
+                        style={{ fontSize: "34px" }}
+                      />
+                    }
+                    iconTitle="Drop Area"
+                    twoToneColor="#12c2e9"
+                    value="direct-drop-area"
+                    onClick={() =>
+                      window.open(
+                        "https://www.npmjs.com/package/direct-drop-area",
+                        "_blank"
+                      )
+                    }
+                  />
+                </div>
+                <div className="justify-start pb-5">
+                  <NewIcon
+                    custom={
+                      <FileZipOutlined
+                        theme="twoTone"
+                        twoToneColor="#12c2e9"
+                        style={{ fontSize: "34px" }}
+                      />
+                    }
+                    iconTitle="Location"
+                    twoToneColor="#12c2e9"
+                    value="get-detailed-location"
+                    onClick={() =>
+                      window.open(
+                        "https://www.npmjs.com/package/get-detailed-location",
+                        "_blank"
+                      )
+                    }
+                  />
+                </div>
+              </div>
+            </div>
           </div>
+        </div>
         <div ref={this.focusMySkills}>
           <SkillsList />
         </div>
--- src/components/Home/components/StudyTimeLine/index.js
+++ src/components/Home/components/StudyTimeLine/index.js
@@ -1,91 +1,80 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 const values = [
   {
     year: 2010,
-    degree: 'Schooling',
-    name: 'Seventh Day Adventist',
+    degree: "Schooling",
+    name: "Seventh Day Adventist",
   },
   {
     year: 2012,
-    degree: 'Intermediate',
-    name: 'New Model High School',
-  },
-  {
-    year: 2013,
-    degree: '',
-    name: '',
+    degree: "Intermediate",
+    name: "New Model High School",
   },
   {
     year: 2015,
-    degree: 'Bachlor\'s in Computer Application',
-    name: 'C.B.P.C.C',
-    duration: '2003-2015',
+    degree: "Bachlor's in Computer Application",
+    name: "C.B.P.C.C",
+    duration: "2003-2015",
   },
   {
     year: 2015,
-    degree: 'Software engineer',
-    name: 'Study Field Technologies',
-  },
-  {
-    year: 2016,
-    degree: '',
-    name: '',
+    degree: "Jr.Software engineer",
+    name: "Study Field Technologies",
   },
   {
     year: 2017,
-    degree: 'MSc. Information and Communication Technology',
-    name: 'VNSGU',
-    duration: '2015-2017',
+    degree: "MSc. Information and Communication Technology",
+    name: "VNSGU",
+    duration: "2015-2017",
   },
   {
     year: 2017,
-    degree: 'Software engineer',
-    name: 'Copart',
-  },
-  {
-    year: 2018,
-    degree: '',
-    name: '',
+    degree: "Software engineer",
+    name: "Copart",
   },
   {
     year: 2020,
-    degree: 'Senior Software engineer',
-    name: 'Copart Inc',
-    duration: '2011-2023',
+    degree: "Senior Software engineer",
+    name: "Copart Inc",
+    duration: "2011-2023",
   },
   {
     year: 2023,
-    degree: 'Frontend engineer',
-    name: 'United Health Group (Optum)',
-    duration: '2023',
+    degree: "Frontend engineer",
+    name: "United Health Group (Optum)",
+    duration: "2023",
   },
   {
     year: 2024,
-    degree: 'Senior Software engineer',
-    name: 'Walmart',
-    duration: '2024-Present',
-  }
+    degree: "Senior Software engineer",
+    name: "Walmart",
+    duration: "2024-Present",
+  },
 ];
 
-const Category = ({ categoryName }: string) => <h3 className="fontColor">{categoryName}</h3>;
+const Category = ({ categoryName }: string) => (
+  <h3 className="fontColor">{categoryName}</h3>
+);
 
 export default class StudyTimeLine extends Component {
   render() {
     return (
       <div className="w-full flex flex-col pb-5 items-center flex-start">
         <div className="pb-10">
-          <Category
-            categoryName="My Education + Experience"
-          />
+          <Category categoryName="My Education + Experience" />
         </div>
         <div className="flex border linearGradient rounded-full h-2 flex flex-row flex-item w-3/4">
-          {values.map(value => (
+          {values.map((value) => (
             <div className="flex flex-col items-center w-full justify-center">
-              <div className={`flex w-4 h-4 rounded-full bg-white border border-red items-center justify-center ${value.degree === '' ? 'pt-2' : 'pt-8'}`}>
+              <div
+                className={`flex w-4 h-4 rounded-full bg-white border border-red items-center justify-center ${
+                  value.degree === "" ? "pt-2" : "pt-8"
+                }`}
+              >
                 <div className="flex flex-col pt-20">
                   <p className="pt-3 text-xs">{value.year}</p>
-                  {value.degree !== '' ? (
+                  {value.degree !== "" ? (
                     <p className="text-xs">
                       {`${value.degree}- ${value.name}`}
                     </p>
