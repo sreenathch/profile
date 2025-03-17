767107d28267a2a502d76b44598d53dd471d4cfd
sreenathch <sreenath.chakinala@copart.com>
2025-03-17 16:05:10 -0500

    Fix Terser plugin issue in webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,47 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
-  entry: './index.js',
+  entry: "./index.js",
   output: {
-    path: path.join(__dirname, '/build'),
-    filename: 'index_bundle.js',
+    path: path.join(__dirname, "/build"),
+    filename: "index_bundle.js",
   },
   devServer: {
     inline: true,
     port: 8082,
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          compress: {
+            drop_console: true,
+          },
+          format: {
+            comments: false,
+          },
+        },
+      }),
+    ],
   },
   module: {
     rules: [
       {
         test: /\.jsx?$/,
         exclude: /node_modules/,
-        loader: 'babel-loader',
+        loader: "babel-loader",
         query: {
-          presets: ['babel-preset-es2015', 'es2015', 'react', 'stage-2'],
+          presets: ["babel-preset-es2015", "es2015", "react", "stage-2"],
         },
       },
       {
         test: /\.(png|jpe?g|gif)$/,
         use: [
           {
-            loader: 'file-loader',
+            loader: "file-loader",
             options: {},
           },
         ],
@@ -34,26 +50,26 @@ module.exports = {
         test: /\.(pcss|css)$/,
         use: [
           {
-            loader: 'css-loader',
+            loader: "css-loader",
             options: {
               modules: true,
               importLoaders: 1,
-              localIdentName: '[path]-[name]-[local]'
-            }
+              localIdentName: "[path]-[name]-[local]",
+            },
           },
           {
-            loader: 'postcss-loader',
+            loader: "postcss-loader",
             options: {
-              sourceMap: true
-            }
-          }
-        ]
-      }
+              sourceMap: true,
+            },
+          },
+        ],
+      },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './index.html',
+      template: "./index.html",
     }),
   ],
 };
