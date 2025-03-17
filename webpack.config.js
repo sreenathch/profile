12dc9f82623bf412d82ea247c0ed454b6473d603
sreenathch <sreenath.chakinala@copart.com>
2025-03-17 17:00:35 -0500

    Enhance webpack configuration for production mode and optimize chunk splitting; update GitHub Actions workflow to use Node.js 16.x and adjust build environment variables.

--- webpack.config.js
+++ webpack.config.js
@@ -11,8 +11,29 @@ module.exports = {
     inline: true,
     port: 8082,
   },
+  mode: "production",
   optimization: {
     minimize: true,
+    splitChunks: {
+      chunks: "all",
+      minSize: 20000,
+      maxSize: 244000,
+      minChunks: 1,
+      maxAsyncRequests: 30,
+      maxInitialRequests: 30,
+      cacheGroups: {
+        defaultVendors: {
+          test: /[\\/]node_modules[\\/]/,
+          priority: -10,
+          reuseExistingChunk: true,
+        },
+        default: {
+          minChunks: 2,
+          priority: -20,
+          reuseExistingChunk: true,
+        },
+      },
+    },
   },
   module: {
     rules: [
