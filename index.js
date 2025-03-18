d550cf1f651765ca079e07b278c4bd5fa244da61
sreenathch <sreenath.chakinala@copart.com>
2025-03-18 16:39:55 -0500

    Fix CSS loading with correct postcss-loader version

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import "./styles.css";
 
-import combineReducers from './src/reducers';
+import combineReducers from "./src/reducers";
 
-import HomeView from './src/components/Home';
+import HomeView from "./src/components/Home";
 
 const store = createStore(combineReducers, applyMiddleware(thunk));
 
 ReactDOM.render(
-  (
-    <Provider store={store}>
-      <HomeView />
-    </Provider>
-  ), document.getElementById('app'),
+  <Provider store={store}>
+    <HomeView />
+  </Provider>,
+  document.getElementById("app")
 );
